17098d7a2abfc137978eef42342247ed1111adf3
PeteMurr <pmurriner@gmail.com>
2024-11-12 09:39:33 -0500

    Update MultTableRunner.java

--- MultTableRunner.java
+++ MultTableRunner.java
@@ -2,11 +2,19 @@
 //www.apluscompsci.com
 //Name
 
+import java.util.Scanner;
+
 public class MultTableRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("enter a number");
+		int value = keyboard.nextInt();
+		System.out.print("enter another number");
+		int times = keyboard.nextInt();
+		MultTable Multvar = new MultTable(value, times);
+		Multvar.printTable();
 		
 	}
 }
